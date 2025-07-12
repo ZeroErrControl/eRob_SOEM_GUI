5077a5de165f3292772c19ee1e708d9ab879865a
ZeroErrControl <overseas_dpt@zeroerr.cn>
2025-07-12 17:03:59 +0800

    20250712-2

--- build/src/CMakeFiles/ethercat_monitor_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ethercat_monitor_autogen"
-  "ethercat_monitor_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ethercat_monitor_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
